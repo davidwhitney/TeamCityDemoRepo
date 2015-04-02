d1e558d880926efd5568921017e9abad268c9bb3
David Whitney <david.whitney@euromoneyplc.com>
2015-04-02 13:31:49 +0100

    Tests

--- tests/SomeClassLibrary.Test.Unit/Class1Tests.cs
+++ tests/SomeClassLibrary.Test.Unit/Class1Tests.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace SomeClassLibrary.Test.Unit
 {
+    [TestFixture]
     public class Class1Tests
     {
+        [Test]
+        public void AssertTrue()
+        {
+            Assert.That(true, Is.True);
+        }
+
+        [Test]
+        public void FailTest()
+        {
+            Assert.That(true, Is.False);
+        }
     }
 }
